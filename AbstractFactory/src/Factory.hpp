51bd8deb9ce55d6aee986e13da7a2c3cd8d76b09
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-04-12 21:01:33 +0300

    Factory: easier usage (cast happens automagically)

--- AbstractFactory/src/Factory.hpp
+++ AbstractFactory/src/Factory.hpp
@@ -1,31 +1,32 @@
 #if 0 //Factory for objects derived from same Base class
 - maintains association between a key and a creator for a type
 - returns a pointer to an instance of type associated with a key
-- can be used for Abstract Factory Pattern by using AbstractFactory interface as Base
+- can be used for Abstract Factory Pattern by passing AbstractFactory interface as Base
 #endif
 #pragma once
 #include <functional>
 #include<map>
 
-template<typename Key, typename Base>class Factory{
+template<typename Key, typename Base> class Factory{
 public:
     template<typename T> void registerType(
-        Key                    key, 
-        std::function<Base*()> creator=_creator<T>//could be function or lambda (bind works but discouraged since lambdas)
-    )
-    {
+        Key                    key, //ToDo: can deduce the key from T?
+        std::function<Base*()> creator=_creator<T>//function or lambda (bind works but discouraged since lambdas)
+    ){
         _creatorsMap[key] = creator;
     }
 
-    template<typename T> T* create(Key key){
-        if(!_creatorsMap[key]){
-            return nullptr;
-        }
-        return (T*)_creatorsMap[key]();
+    Factory& operator()(Key key){
+        auto f = _creatorsMap[key];
+        _base = (f ? f() : nullptr);
+        return *this;
     }
+
+    template<typename T> operator T(){return (T)_base;}
 
 private:
     std::map<Key, std::function<Base*()>> _creatorsMap;
+    Base*                                 _base;
 
     template<typename T> static Base* _creator(){return new T;}//default creator
 };
--- AbstractFactory/src/main.cpp
+++ AbstractFactory/src/main.cpp
@@ -2,75 +2,84 @@
 #include <stdio.h>
 
 //--------------------------------------------------------------------------------
-class Base{
+class T0{
 public:
     int nr;
-    Base(int nr=0): nr(nr){}
-    virtual ~Base(){printf("%s() this=%p nr=%d\n", __FUNCTION__, this, nr);}
-    virtual const char* cls(){return nullptr;}//class ID
+    T0(int nr=0): nr(nr){}
+    virtual ~T0(){printf("%s() this=%p nr=%4d\n", __FUNCTION__, this, nr);}
+    virtual const char* cls(){return "T0";}//class ID
 };
 
 //--------------------------------------------------------------------------------
-class Derived: public Base{
+class T1: public T0{
 public:
-    Derived(int nr=1): Base(nr){printf("%s(%d)\n", __FUNCTION__, nr);}
-    const char* cls() override {return "Derived";}
-    static Base* create(){return new Derived(1973);}//creator function
+    T1(int nr=1): T0(nr){printf("%s(%4d) this=%p\n", __FUNCTION__, nr, this);}
+    const char* cls() override {return "T1";}
 };
 
 //--------------------------------------------------------------------------------
-class Derived2: public Base{
+class T2: public T0{
 public:
-    Derived2(int nr=2): Base(nr){printf("%s(%d)\n", __FUNCTION__, nr);}
-    const char* cls() override {return "Derived2";}
+    T2(int nr=2): T0(nr){printf("%s(%4d) this=%p\n", __FUNCTION__, nr, this);}
+    const char* cls() override {return "T2";}
+    static T0* create(){return new T2(1973);}//creator function
 };
 
 //--------------------------------------------------------------------------------
-class DerivedCreator{//could be a life cycle manager for Derived objects
+class T2Creator{//could be a life cycle manager for derived objects
 public:
-    DerivedCreator(){}
-    Derived* create(){
-        static Derived singleton(2020);
+    T2* create(){
+        static T2 singleton(2020);//destroyed @ exit (uncontrolled order)
         return &singleton;
     }
 };
 
 //--------------------------------------------------------------------------------
 int main(int /*argc*/, char** /*argv*/){
-    Factory<std::string, Base> factory;//factory for descendants of Base
+    Factory<std::string, T0> factory;//factory for descendants of T0
 
-    //factory.registerClass<Base>("Base");
-    factory.registerType<Derived>("Derived");
-    factory.registerType<Derived>("Derived_function", &Derived::create);//same class as above but different name and creator
-    factory.registerType<Derived>("Derived_lambda"  , [](){return new Derived(2018);});//same class as above but different name and lambda creator
-    DerivedCreator derivedCreator;
-    factory.registerType<Derived>("Derived_object"  , [&derivedCreator](){return derivedCreator.create();});//lambda + object creator
-    factory.registerType<Derived>("Derived_bind"    , std::bind(&DerivedCreator::create, derivedCreator));//using std::bind works but is discouraged since lambda
-    factory.registerType<Derived2>("Derived2");
+    //factory.registerClass<T0>("T0");
+    factory.registerType<T1>("T1");//using default creator for T1
+    factory.registerType<T2>("T2");//using default creator for T2
+    factory.registerType<T2>("T2_func", &T2::create);//function creator
+    factory.registerType<T2>("T2_lambda"  , [](){return new T2(2018);});//lambda creator
+    T2Creator t2Creator;
+    factory.registerType<T2>("T2_object"  , [&t2Creator](){return t2Creator.create();});//lambda + object creator
+    factory.registerType<T2>("T2_bind"    , std::bind(&T2Creator::create, t2Creator));//std::bind works but is discouraged since lambda
 
-    auto b = factory.create<Base>("Base");//"Base" not registered
-    if(!b){printf("[%s()] \"%s\" not registered\n", __FUNCTION__, "Base");}
+    T0* b = factory("T0");
+    if(!b){printf("[%s()] \"%s\" not registered\n", __FUNCTION__, "T0");}
 
-    struct{
-        bool  deletable;
-        Base* ptr;
-    } arr[] = {//array of objects derived from Base created via factory
-        {true , factory.create<Derived >("Derived"          )},
-        {true , factory.create<Derived2>("Derived_function" )}, 
-        {true , factory.create<Derived2>("Derived_lambda"   )}, 
-        {false, factory.create<Derived2>("Derived_object"   )},//no delete because is a singleton 
-        {false, factory.create<Derived2>("Derived_bind"     )},//no delete because is a singleton 
-        {true , factory.create<Derived2>("Derived2"         )}, 
-    };
+    {//simple usage
+        printf("======== simple usage:\n");
+        T1* t1 = factory("T1");//no need to cast explicitly, it happens automagically
+        delete t1;
+        T2* t2 = factory("T2");
+        delete t2;
+    }
+    {//array
+        printf("======== array:\n");
+        struct{
+            bool deletable;
+            T0*  ptr;
+        } arr[] = {//array of objects derived from Base created via factory
+            {true , factory("T1"            )},
+            {true , factory("T2"            )}, 
+            {true , factory("T2_func"       )}, 
+            {true , factory("T2_lambda"     )}, 
+            {false, factory("T2_object"     )},//no delete because it is a singleton 
+            {false, factory("T2_bind"       )},//no delete because it is a singleton 
+        };
 
-    for(size_t i=0; i< sizeof(arr)/sizeof(arr[0]); ++i){
-        if(arr[i].ptr){
-            printf("%zd: %s nr=%d\n", i, arr[i].ptr->cls(), arr[i].ptr->nr);
-        }else{
-            printf("%zd: nullptr\n", i);
-        }
-        if(arr[i].deletable){
-            delete arr[i].ptr;
+        for(size_t i=0; i< sizeof(arr)/sizeof(arr[0]); ++i){
+            if(arr[i].ptr){
+                printf("%zd: this=%p %s nr=%d\n", i, arr[i].ptr, arr[i].ptr->cls(), arr[i].ptr->nr);
+            }else{
+                printf("%zd: nullptr\n", i);
+            }
+            if(arr[i].deletable){
+                delete arr[i].ptr;
+            }
         }
     }
 
