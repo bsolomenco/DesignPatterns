a42ebf89606b1dd07a7827ef265f7f546211e6e7
Bogdan SOLOMENCO <bsolomenco@gmail.com>
2018-04-13 10:28:21 +0300

    Factory: comments

--- AbstractFactory/src/Factory.hpp
+++ AbstractFactory/src/Factory.hpp
@@ -10,7 +10,7 @@
 template<typename Key, typename Base> class Factory{
 public:
     template<typename T> void registerType(
-        Key                    key, //ToDo: can deduce the key from T?
+        Key                    key, //ToDo: can deduce the key from T? then what about different keys for same type?
         std::function<Base*()> creator=_creator<T>//function or lambda (bind works but discouraged since lambdas)
     ){
         _creatorsMap[key] = creator;
@@ -30,3 +30,22 @@ private:
 
     template<typename T> static Base* _creator(){return new T;}//default creator
 };
+
+#if 0 // USAGE
+class T0{...}; //base class
+class Ti: public T0{...}; //derived classes; i>0
+Factory<std::string, T0> factory;
+
+factory.registerType<T1>("T2");//using default creator
+factory.registerType<T2>("T2");
+factory.registerType<T2>("T2_create", &T2::create);//assume there is a static T0* T2::create()
+factory.registerType<T2>("T2_lambda", [](){return new T2(...);}]);//lambda creator
+//...
+
+T1* t1 = factory("T1");//NOTE: no explicit cast needed!
+T2* t2 = factory("T2");
+T2* t3 = factory("T2_create");
+T2* t4 = factory("T2_lambda");
+
+//destroy instances (where necessary)
+#endif
